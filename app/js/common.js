89f8f402fa4b7b626002b72eaedfafee2863d8a3
Viktoria <zaharova181114@gmail.com>
2022-02-05 21:53:58 +0200

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,34 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+// header fixed
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
 });
+
+// slick
+
+$(document).ready(function () {
+    $('.production-slider').each(function () {
+        $(this).slick({
+            slidesToShow: 1,
+            fade: true,
+            appendArrows: $(this).parents('.slider').find('.slider-arrows'),
+            prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+            nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+        });
+    });
+
+    //auto counter total//
+    let homeSlider = $('.production-slider');
+
+    $('.counter-slide__default').text("/ " + homeSlider.slick("getSlick").slideCount);
+
+    homeSlider.on('afterChange', function (event, slick, currentSlide) {
+        $(".counter-slide__cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
+    });
+
+});
+
+
